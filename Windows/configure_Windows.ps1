07a863ed1e2d30158edf562e0010a80f32538ec7
DavTheHuman <29121925+DavTheHuman@users.noreply.github.com>
2018-11-03 17:17:04 -0400

    Add setup file for Windows

---
+++ Windows/configure_Windows.ps1
@@ -0,0 +1,9 @@
+$invocation = (Get-Variable MyInvocation).Value
+$dirpath = Split-Path $invocation.MyCommand.Path
+$mcpath = $dirpath + '\index.js'
+
+if (!(Test-Path -Path $profile)) {New-Item -ItemType File -Path $profile -Force}
+
+Add-Content $profile “`r`nfunction mcat {`$input | node $mcpath `$args}”
+
+function mcat {$input | node $mcpath $args}
