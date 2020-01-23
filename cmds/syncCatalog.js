216832c78189d0db611a2050a2bccfcef039d7a9
DavTheHuman <29121925+DavTheHuman@users.noreply.github.com>
2020-01-23 15:08:38 -0500

    Syncing of release catalog
    Use --releases to sync release catalog.
    Release catalog sometimes does not return the requested number of items, so the skip number is now iterated by the amount recieved each request.

--- cmds/syncCatalog.js
+++ cmds/syncCatalog.js
@@ -3,16 +3,16 @@ const _ = require('lodash')
 const monstercat = require('../lib/monstercat')
 
 const PAGE_SIZE = 50
-const getPage = (pageNum, results, done)=> {
+const getPage = (skip, isReleases, results, done)=> {
   if (typeof(results) == 'function') {
     done = results
     results = []
   }
-  console.log(`-- 💌  Requesting Monstercat Catalog (page ${pageNum}).`)
-  monstercat.request('GET', `/v2/catalog/browse?limit=${PAGE_SIZE}&skip=${pageNum * PAGE_SIZE}`, (err, res, body)=> {
+  console.log(`-- 💌  Requesting Monstercat Catalog (page ${skip/PAGE_SIZE}).`)
+  monstercat.request('GET', `/v2/${isReleases ? 'releases' : 'catalog/browse'}?limit=${PAGE_SIZE}&skip=${skip}`, (err, res, body)=> {
     if (err) return done(err, null, false)
     const parsedBody = JSON.parse(body)
-    console.log(`-- ⬇️  Got ${parsedBody.results.length} tracks from Monstercat Catalog (page ${pageNum}).`)
+    console.log(`-- ⬇️  Got ${parsedBody.results.length} ${isReleases ? 'releases' : 'tracks'} from Monstercat Catalog (page ${skip/PAGE_SIZE}).`)
 
     // Concat; less re-allocation
     _.each(parsedBody.results, (e)=> results.push(e))
@@ -21,12 +21,12 @@ const getPage = (pageNum, results, done)=> {
     if (parsedBody.results.length == 0) return done(null, results)
 
     // Get next pages recursively; Catalog is small enough this is safe
-    return getPage(pageNum + 1, results, done)
+    return getPage(skip + parsedBody.results.length, isReleases, results, done)
   })
 
 }
 const syncCatalog = (dbg, args, done) => {
-  getPage(0, (err, results)=> {
+  getPage(0, _.includes(args, '--releases'), (err, results)=> {
     if (err) return done(err)
     console.log(`-- 📝  Syncing Monstercat Catalog with ${results.length} entries to FS cache.`)
     fs.writeFile(monstercat.CATALOG_PATH, JSON.stringify(results), (err) => {
